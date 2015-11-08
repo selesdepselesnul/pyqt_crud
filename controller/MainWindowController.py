8bfa9fb0b2d9a65cbe54f9fc23a9ccd4f926aeca
selesdepselesnul <selesdepselesnul@gmail.com>
2015-11-08 12:04:01 +0700

    add filtering for students

--- controller/MainWindowController.py
+++ controller/MainWindowController.py
@@ -10,37 +10,51 @@ class MainWindowController(QWidget):
         super().__init__()
 
         self.__STUDENTS_DATA_FILE = 'Students.dat'
+        self.__students = []
         self.ui = uic.loadUi('ui/main_window.ui', self)
         self.ui.submiting_button.clicked.connect(
             self.on_submiting_data)
-
+        self.ui.students_filtering_combo_box.activated[str].connect(
+            self.on_filtering_students
+        )
 
         self.update_students_table_widget()
+
+    def on_filtering_students(self, choosen):
+        def filtering_student(predicate):
+            filtered_student = list(filter(predicate,
+                                           self.__students))
+            self.ui.students_table_widget.clearContents()
+            self.__add_to_students_table_widget(filtered_student)
+
+        if str(choosen) == 'Display Active Data':
+            filtering_student(lambda x: bool(x.is_active) == True)
+        elif str(choosen) == 'Display Deleted Data':
+            filtering_student(lambda x: bool(x.is_active) == False)
+            print('You choose display deleted data')
+        else:
+            self.update_students_table_widget()
+
+    def __add_to_students_table_widget(self, students):
+        self.ui.students_table_widget.setRowCount(len(self.__students))
+        for i in range(len(students)):
+            self.ui.students_table_widget.setItem(i, 0, QTableWidgetItem(
+                                                          students[i].student_id))
+            self.ui.students_table_widget.setItem(i, 1, QTableWidgetItem(
+                                                          students[i].name))
+            self.ui.students_table_widget.setItem(i, 2, QTableWidgetItem(
+                                                          students[i].address))
+            self.ui.students_table_widget.setItem(i, 3, QTableWidgetItem(
+                                                          str(students[i].
+                                                              is_active)))
 
     def update_students_table_widget(self):
 
         students_path = Path(self.__STUDENTS_DATA_FILE)
         if students_path.exists():
-            students = list(
+            self.__students = list(
                 pickle.load(open(self.__STUDENTS_DATA_FILE, 'rb')))
-            self.ui.students_table_widget.setRowCount(len(students))
-            for i in range(len(students)):
-                self.ui.students_table_widget.setItem(i, 0,
-                                                      QTableWidgetItem(
-                                                          students[
-                                                              i].student_id))
-                self.ui.students_table_widget.setItem(i, 1,
-                                                      QTableWidgetItem(
-                                                          students[
-                                                              i].name))
-                self.ui.students_table_widget.setItem(i, 2,
-                                                      QTableWidgetItem(
-                                                          students[
-                                                              i].address))
-                self.ui.students_table_widget.setItem(i, 3,
-                                                      QTableWidgetItem(
-                                                          str(students[
-                                                              i].is_active)))
+            self.__add_to_students_table_widget(self.__students)
 
     def on_submiting_data(self):
         student = Student(self.ui.id_line_edit.text(),
