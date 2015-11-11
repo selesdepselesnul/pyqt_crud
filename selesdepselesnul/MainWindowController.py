a13be4995ad6afc17fc84397e3b54025821061f6
selesdepselesnul <selesdepselesnul@gmail.com>
2015-11-12 05:07:56 +0700

    refactor

--- selesdepselesnul/MainWindowController.py
+++ selesdepselesnul/MainWindowController.py
@@ -13,15 +13,14 @@ from selesdepselesnul.Student import Student
 
 
 class MainWindowController(QWidget):
-    FILE_FILTERING_FORMAT = "lll File (*lll)"
-    DEFAULT_FILE = 'Students'
-    FILE_EXT = '.lll'
+    __FILE_FILTERING_FORMAT = "lll File (*lll)"
+    __DEFAULT_FILE = 'Students'
+    __FILE_EXT = '.lll'
+    __STUDENTS_DATA_FILE = __DEFAULT_FILE + __FILE_EXT
 
     def __init__(self):
         super().__init__()
 
-        self.__STUDENTS_DATA_FILE = MainWindowController.DEFAULT_FILE \
-                                    + MainWindowController.FILE_EXT
         self.__students = []
         self.ui = uic.loadUi('selesdepselesnul/main_window.ui', self)
         self.ui.student_tab.setTabText(0, 'Input Mahasiswa')
@@ -54,21 +53,22 @@ class MainWindowController(QWidget):
             self.on_packing_student_button_clicked
         )
 
-        self.update_students_table_widget()
+        self.__update_students_table_widget()
 
     def on_packing_student_button_clicked(self):
         tuple_of_selected_file = QFileDialog.getSaveFileName(
-            self.ui, "Save lll File", MainWindowController.DEFAULT_FILE +
-            "-bak" + MainWindowController.FILE_EXT, MainWindowController
-            .FILE_FILTERING_FORMAT)
+            self.ui, "Save lll File", MainWindowController.__DEFAULT_FILE +
+            "-bak" + MainWindowController.__FILE_EXT, MainWindowController
+                .__FILE_FILTERING_FORMAT)
 
         if tuple_of_selected_file != ('', ''):
             pickle.dump(self.__students, open(tuple_of_selected_file[0], 'wb'))
 
     def on_loading_student_button_clicked(self):
         tuple_of_selected_file = QFileDialog.getOpenFileName(
-            self.ui, "Load lll file", 'Sesuatu' + MainWindowController.FILE_EXT,
-            MainWindowController.FILE_FILTERING_FORMAT)
+            self.ui, "Load lll file", 'Sesuatu' + MainWindowController
+                                      .__FILE_EXT, MainWindowController
+                                      .__FILE_FILTERING_FORMAT)
         if tuple_of_selected_file != ('', ''):
             students = list(pickle.load(open(tuple_of_selected_file[0], 'rb')))
             self.__add_to_students_table_widget(students)
@@ -76,11 +76,11 @@ class MainWindowController(QWidget):
     def on_typing_filtering_id(self):
         filtered_id = str(self.ui.filtering_by_id_line_edit.text())
         filtered_students = list(filter(
-            lambda x: re.match(filtered_id + "\d*" ,
+            lambda x: re.match(filtered_id + "\d*",
                                x.student_id), self.__students))
         self.__add_to_students_table_widget(filtered_students)
 
-    def change_data_by_id(self, item, new_value, action):
+    def __change_data_by_id(self, item, new_value, action):
         student_id = self.ui.students_table_widget.item(
                     item.row(), 0).text()
         for i, student in enumerate(self.__students):
@@ -89,7 +89,7 @@ class MainWindowController(QWidget):
 
     def on_student_end_editing(self, _, prev):
         def change_value(action):
-            self.change_data_by_id(prev, prev.text(), action)
+            self.__change_data_by_id(prev, prev.text(), action)
         if prev is not None:
             if prev.column() == 2:
                 def address_at(i, new_value):
@@ -103,13 +103,12 @@ class MainWindowController(QWidget):
             pickle.dump(self.__students, open(self.__STUDENTS_DATA_FILE, 'wb'))
 
     def on_student_data_clicked(self, item):
-        print('Double click')
 
-        def change_status_at(i, new_value):
+        def status_at(i, new_value):
             self.__students[i].status = new_value
 
         def change_status(new_value):
-            self.change_data_by_id(item, new_value, change_status_at)
+            self.__change_data_by_id(item, new_value, status_at)
 
         if item.column() == 3:
 
@@ -124,23 +123,22 @@ class MainWindowController(QWidget):
 
             pickle.dump(self.__students, open(self.__STUDENTS_DATA_FILE, 'wb'))
 
-    def on_filtering_students(self, choosen):
-        def filtering_student(predicate):
+    def on_filtering_students(self, selected_mode):
+        def filter_student(predicate):
             filtered_student = list(filter(predicate,
                                            self.__students))
             self.ui.students_table_widget.clearContents()
             self.__add_to_students_table_widget(filtered_student)
 
-        if str(choosen) == 'Display Active Data':
-            filtering_student(lambda x: x.status == Student.ACTIVE)
-        elif str(choosen) == 'Display Deleted Data':
-            filtering_student(lambda x: x.status == Student.DEACTIVE)
-            print('You choose display deleted data')
+        if selected_mode == 'Display Active Data':
+            filter_student(lambda x: x.status == Student.ACTIVE)
+        elif selected_mode == 'Display Deleted Data':
+            filter_student(lambda x: x.status == Student.DEACTIVE)
         else:
-            self.update_students_table_widget()
+            self.__update_students_table_widget()
 
     def __add_to_students_table_widget(self, students):
-        def uneditable_item_widget(item_widget):
+        def un_editable_item_widget(item_widget):
             item = QTableWidgetItem(item_widget)
             item.setFlags(item.flags() ^ Qt.ItemIsEditable)
             return item
@@ -148,24 +146,25 @@ class MainWindowController(QWidget):
         self.ui.students_table_widget.setRowCount(len(students))
         for i, student in enumerate(students):
             self.ui.students_table_widget.setItem(i, 0, QTableWidgetItem(
-                                                          uneditable_item_widget(
+                                                        un_editable_item_widget(
                                                             student.student_id
-                                                          )))
+                                                        )))
             self.ui.students_table_widget.setItem(i, 1, QTableWidgetItem(
                                                           student.name))
             self.ui.students_table_widget.setItem(i, 2, QTableWidgetItem(
                                                           student.address))
-            status_item = uneditable_item_widget(student.status)
+            status_item = un_editable_item_widget(student.status)
             if student.status == Student.ACTIVE:
                 status_item.setBackground(QColor('green'))
             else:
                 status_item.setBackground(QColor('red'))
             self.ui.students_table_widget.setItem(i, 3, status_item)
 
-    def update_students_table_widget(self):
-        if Path(self.__STUDENTS_DATA_FILE).exists():
+    def __update_students_table_widget(self):
+        if Path(MainWindowController.__STUDENTS_DATA_FILE).exists():
             self.__students = list(
-                pickle.load(open(self.__STUDENTS_DATA_FILE, 'rb')))
+                pickle.load(open(MainWindowController.__STUDENTS_DATA_FILE,
+                                 'rb')))
             self.__add_to_students_table_widget(self.__students)
 
     def on_submitted_data(self):
@@ -182,5 +181,4 @@ class MainWindowController(QWidget):
                                                  'rb')))
             student_list.append(student)
             pickle.dump(student_list, open(self.__STUDENTS_DATA_FILE, 'wb'))
-            self.update_students_table_widget()
-
+            self.__update_students_table_widget()
